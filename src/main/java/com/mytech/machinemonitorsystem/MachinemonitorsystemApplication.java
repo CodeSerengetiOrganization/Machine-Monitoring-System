936c61a68d9551b471d62cd820e3544c041d8816
CodeSerengetiOrg <codeserengetiorg@gmail.com>
2025-03-28 01:22:12 -0400

    Initialized the repository with SpringBoot3.4

---
+++ src/main/java/com/mytech/machinemonitorsystem/MachinemonitorsystemApplication.java
@@ -0,0 +1,13 @@
+package com.mytech.machinemonitorsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachinemonitorsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachinemonitorsystemApplication.class, args);
+	}
+
+}

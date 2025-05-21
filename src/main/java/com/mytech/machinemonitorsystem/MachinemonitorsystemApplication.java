e92e02024d24d02f90ff8484b4c6e91765babae1
CodeSerengetiOrg <codeserengetiorg@gmail.com>
2025-05-21 13:54:16 -0400

    Code clean up, deleted commented code, packaged into war and deploy into JBoss, works fine.

--- src/main/java/com/mytech/machinemonitorsystem/MachinemonitorsystemApplication.java
+++ src/main/java/com/mytech/machinemonitorsystem/MachinemonitorsystemApplication.java
@@ -1,19 +1,12 @@
 package com.mytech.machinemonitorsystem;
 
-//import com.mytech.machinemonitorsystem.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-import java.util.List;
-
-//@SpringBootApplication(excludeName = {"org.springframework.boot.autoconfigure.web.servlet.error.ErrorPageFilterAutoConfiguration"})
-//@ComponentScan("com.mytech.machinemonitorsystem")
 @SpringBootApplication
-//@ServletComponentScan // Add this annotation
 public class MachinemonitorsystemApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
